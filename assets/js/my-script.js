74a7e72e24a130f21c033d12aea4cff24c604add
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-04-22 06:36:58 +0530

    Configured sections in fullpage.js

--- assets/js/my-script.js
+++ assets/js/my-script.js
@@ -1,6 +1,7 @@
 // Fullpage JS
 new fullpage("#fullpage", {
   // Options here
+  anchors: ['section-1', 'section-2', 'section-3', 'section-4','section-5', 'section-6', 'section-7','section-8'],
   navigation: true,
   onLeave: () => {
     checkAboutSectionVisiblity();
